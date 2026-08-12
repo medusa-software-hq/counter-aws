1ef1a1ba161ac49c8bf6dfde14532260cc5dd39c
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-08-12 16:56:11 +0200

    Revert "Merge pull request #10 from medusa-software-hq/cognito-jwt-verifier"
    This reverts commit 40ec7169368ed0fb766c357d5b892d84e95d29fa, reversing
    changes made to d3c84f040529ae55e1534b58b8905ccfbe9f2775.

--- backend/api/shared/src/main/kotlin/software/medusa/counter/server/CounterPrincipal.kt
+++
@@ -1,7 +0,0 @@
-package software.medusa.counter.server
-
-/** The authenticated caller, projected from a verified Cognito ID token. */
-data class CounterPrincipal(
-    val email: String,
-    val groups: List<String>,
-)
